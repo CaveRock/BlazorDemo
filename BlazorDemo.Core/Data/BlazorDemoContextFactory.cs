0aeba41999f373e4aa1e049a98a35c5dd71e8981
Oliver Shepherd <oliver@bitdirect.co.za>
2023-08-10 18:33:51 +0200

    context factory added for migrations

---
+++ BlazorDemo.Core/Data/BlazorDemoContextFactory.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorDemo.Core.Data {
+    public class BlazorDemoContextFactory : IDesignTimeDbContextFactory<BlazorDemoContext> {
+
+        public BlazorDemoContext CreateDbContext(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        IConfiguration GetAppConfiguration()
+        {
+            var environmentName =
+                      Environment.GetEnvironmentVariable(
+                          "ASPNETCORE_ENVIRONMENT");
+
+            var dir = Directory.GetParent(AppContext.BaseDirectory);
+
+            if (Environments.Development.Equals(environmentName,
+                StringComparison.OrdinalIgnoreCase))
+            {
+                var depth = 0;
+                do
+                    dir = dir.Parent;
+                while (++depth < 5 && dir.Name != "bin");
+                dir = dir.Parent;
+            }
+            var path = dir.FullName;
+
+            var builder = new ConfigurationBuilder()
+                    .SetBasePath(path)
+                    .AddJsonFile("appsettings.json")
+                    .AddJsonFile($"appsettings.{environmentName}.json", true)
+                    .AddEnvironmentVariables();
+
+            return builder.Build();
+        }
+    }
+}
