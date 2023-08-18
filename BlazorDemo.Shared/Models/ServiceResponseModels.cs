fb4598e6db50300cd4e44dc4c0892162cdc03fb7
CaveRock <oshepherd@gmail.com>
2023-08-18 12:21:19 +0200

    property renaming

--- BlazorDemo.Shared/Models/ServiceResponseModels.cs
+++ BlazorDemo.Shared/Models/ServiceResponseModels.cs
@@ -5,11 +5,28 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace BlazorDemo.Core.Shared.Models {
-    public class ServiceResponseModels {
-        public ServiceResponseModels()
+    public class ServiceActionResult<TActionEntity> {
+        public TActionEntity Entity { get; set; }
+
+        public ValidationResult<TActionEntity> ValidationResult { get; private set; }
+        public ServiceActionResult(TActionEntity entity)
         {
-            ValidationFieldResult<UserModel> res = new ValidationFieldResult<UserModel>("", x => x.Email);
-         
+            ValidationResult = new ValidationResult<TActionEntity>();
+            Entity = entity;
+        }
+
+        public void AddValidationResult(ValidationResult<TActionEntity> validationResult)
+        {
+            ValidationResult = validationResult;
+        }
+    }
+
+    public class ServiceGetResult<TGetEntity> {
+        public TGetEntity? Entity { get; private set; }
+
+        public ServiceGetResult(TGetEntity? entity)
+        {
+            Entity = entity;
         }
     }
 
