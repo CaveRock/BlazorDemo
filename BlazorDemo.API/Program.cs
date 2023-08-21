a79f06194d6291285d87066944a532e6751d07ee
CaveRock <oshepherd@gmail.com>
2023-08-21 12:38:59 +0200

    setting up db context DI auth with JWT setup in program app settings for JWT added

--- BlazorDemo.API/Program.cs
+++ BlazorDemo.API/Program.cs
@@ -1,3 +1,11 @@
+using BlazorDemo.Core.Data.Entities.Identity;
+using BlazorDemo.Core.Server.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -18,7 +26,53 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AuthenticationDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDbContext<BlazorDemoContext>(options => options.UseSqlServer(connectionString, x => x.MigrationsAssembly("BlazorDemo.Core.Server")));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<AuthenticationDbContext>()
+    .AddDefaultTokenProviders();
+
+// Adding Authentication
+builder.Services.AddAuthentication(options => {
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+
+})
+// Adding Jwt Bearer
+.AddJwtBearer(options => {
+    options.SaveToken = true;
+    options.RequireHttpsMetadata = false;
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration.GetSection("JWT").GetValue<string>("ValidAudience"),
+        ValidIssuer = builder.Configuration.GetSection("JWT").GetValue<string>("ValidIssuer"),
+
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration.GetSection("JWT").GetValue<string>("SecretKey")))
+    };
+    options.Events = new JwtBearerEvents //Setup for SignalR hubs
+    {
+        OnMessageReceived = context => {
+            var accessToken = context.Request.Query["access_token"];
+
+            // If the request is for our hub...
+            var path = context.HttpContext.Request.Path;
+            if (!string.IsNullOrEmpty(accessToken) &&
+                (path.StartsWithSegments("/hub")))
+            {
+                // Read the token out of the query string
+                context.Token = accessToken;
+            }
+            return Task.CompletedTask;
+        }
+    };
+});
 
 app.MapControllers();
 
---
+++ BlazorDemo.Core/Data/AuthenticationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using BlazorDemo.Core.Data.Entities.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorDemo.Core.Server.Data {
+    public class AuthenticationDbContext : IdentityDbContext<ApplicationUser> {
+
+        public AuthenticationDbContext(DbContextOptions<AuthenticationDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
